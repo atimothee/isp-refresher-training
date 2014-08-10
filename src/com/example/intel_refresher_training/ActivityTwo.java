c3c2bea2ae919777e6245f96273acbd34857b29b
Timothy Asiimwe <atimothee@gmail.com>
2014-08-10 20:50:02 +0300

    initial commit

---
+++ src/com/example/intel_refresher_training/ActivityTwo.java
@@ -0,0 +1,15 @@
+package com.example.intel_refresher_training;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Timo on 8/10/14.
+ */
+public class ActivityTwo extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
---
+++ src/com/example/intel_refresher_training/MyActivity.java
@@ -0,0 +1,36 @@
+package com.example.intel_refresher_training;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("com.example.intel_refresher_training.ActivityTwo");
+                startActivity(i);
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyActivity.this, "You just clicked button 2", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
