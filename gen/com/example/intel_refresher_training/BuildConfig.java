426d3aba1a98b63540aa1e0c84b7ad7921f4f612
Timothy Asiimwe <atimothee@gmail.com>
2014-08-11 01:37:11 +0300

    added native support

---
+++ gen/com/example/intel_refresher_training/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.intel_refresher_training;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ jni/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := Intel-Refresher-Training
+LOCAL_SRC_FILES := Intel-Refresher-Training.cpp
+
+include $(BUILD_SHARED_LIBRARY)
